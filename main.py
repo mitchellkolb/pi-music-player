93d4844a397f695d6c478822cfd934e48e692544
mitchellkolb <mkolb321@gmail.com>
2024-09-23 23:58:34 -0700

    Its actually possible to use kivy for managing the audio files thank god

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.filechooser import FileChooserIconView
 from kivy.uix.boxlayout import BoxLayout
-import pygame
+from kivy.uix.button import Button
+from kivy.core.audio import SoundLoader
 
-class MP3PlayerApp(App):
+class AudioPlayerApp(App):
     def build(self):
-        self.layout = BoxLayout(orientation='vertical')
+        layout = BoxLayout(orientation='vertical')
         
-        # File chooser to select MP3
-        self.file_chooser = FileChooserIconView(filters=['*.mp3'])
-        self.layout.add_widget(self.file_chooser)
+        # Button to open and play the MP3
+        play_button = Button(text="Play MP3")
+        play_button.bind(on_press=self.play_audio)
+        
+        layout.add_widget(play_button)
+        return layout
+    
+    def play_audio(self, instance):
+        # Load and play the MP3 file
+        sound = SoundLoader.load('path/to/your/file.mp3')
+        if sound:
+            sound.play()
 
-        # Play button
-        self.play_button = Button(text="Play Selected MP3")
-        self.play_button.bind(on_press=self.play_mp3)
-        self.layout.add_widget(self.play_button)
-
-        # Initialize Pygame mixer
-        pygame.mixer.init()
-
-        return self.layout
-
-    def play_mp3(self, instance):
-        selected_file = self.file_chooser.selection
-        if selected_file:
-            pygame.mixer.music.load(selected_file[0])
-            pygame.mixer.music.play()
-
-if __name__ == '__main__':
-    MP3PlayerApp().run()
+if __name__ == "__main__":
+    AudioPlayerApp().run()
